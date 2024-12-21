c0774b359a2c09a6226d1f677b010eaed4ec346d
arth-1 <arthag1304@gmail.com>
2024-12-21 12:38:09 +0530

    why DOES IT NOT WORKKKK!!!!

--- components/PlaceholderDocument.tsx
+++ components/PlaceholderDocument.tsx
@@ -2,7 +2,7 @@
 
 import { PlusCircleIcon } from 'lucide-react'
 import { Button } from './ui/button'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/compat/router'
 
 function PlaceholderDocument() {
     const router = useRouter();
