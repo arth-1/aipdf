bc74797597a2ec3c4b89c59246b9b984db10b0bc
arth-1 <arthag1304@gmail.com>
2024-12-21 12:28:23 +0530

    fixed errors

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -28,7 +28,7 @@ function FileUploader() {
         </> :
         <>
           <CircleArrowDown className='h-20 w-20 animate-ping'/>
-          <p>Drag 'n' drop some files here, or click to select files</p>
+          <p>Drag n drop some files here, or click to select files</p>
         </>
       }
       </div>
